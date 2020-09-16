9c725cba865818c0b4a00452a9f0fa747c368ddc
Harishkumar Gangula <harishg@ilimi.in>
2020-09-16 16:06:30 +0530

    Issue #SB-20001 fix: Added assets copy

--- assets-copy.js
+++ assets-copy.js
@@ -4,7 +4,14 @@ var wrench = require("wrench"),
 var source = "projects/sunbird-pdf-player/node_modules/ngx-extended-pdf-viewer/assets";
 var target = "dist/sunbird-pdf-player/lib/assets/";
 
+var libsource = "projects/sunbird-pdf-player/src/lib/assets";
+
+
 wrench.copyDirSyncRecursive(source, target, {
+  forceDelete: true
+});
+
+wrench.copyDirSyncRecursive(libsource, target, {
   forceDelete: true
 });
 
