c8cd8f031c6d5f6500035e6c90ca4f046a87668c
Gourav More <gourav_m@tekditechnologies.com>
2022-01-28 16:03:48 +0530

    Issue #SB-28363 fix: pdf player test cases improvement

--- projects/sunbird-pdf-player/karma.conf.js
+++ projects/sunbird-pdf-player/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage/sunbird-pdf-player'),
-      reports: ['html', 'lcovonly'],
+      reports: ['text-summary', 'html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
