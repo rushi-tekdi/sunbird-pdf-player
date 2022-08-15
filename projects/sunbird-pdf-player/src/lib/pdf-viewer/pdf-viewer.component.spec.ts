aa9363e39b9cd6e2ec4d993126a99e26fe20daa4
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2022-08-15 23:56:03 +0530

    Issue #KN-23 fix: fixed async issue of angular version update

--- projects/sunbird-pdf-player/src/lib/pdf-viewer/pdf-viewer.component.spec.ts
+++ projects/sunbird-pdf-player/src/lib/pdf-viewer/pdf-viewer.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
 import { PdfViewerComponent } from './pdf-viewer.component';
 
@@ -6,7 +6,7 @@ describe('PdfViewerComponent', () => {
   let component: PdfViewerComponent;
   let fixture: ComponentFixture<PdfViewerComponent>;
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [PdfViewerComponent]
     })
