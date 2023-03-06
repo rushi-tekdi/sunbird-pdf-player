0ee75e76d6a55d8a4ca172f74d17186eea9ca650
Harish Kumar Gangula <harish@sanketika.in>
2023-03-06 14:35:06 +0530

    Issue #KN-832 refactor: Updated the web component folders

--- build-wc.js
+++ build-wc.js
@@ -13,6 +13,9 @@ const build = async () => {
   await concat(files, "web-component/sunbird-pdf-player.js");
   await fs.copy("./dist/pdf-player-wc/assets", "web-component/assets");
   await fs.copy("./dist/pdf-player-wc/styles.css", "web-component/styles.css")
+  await concat(files, "web-component-demo/sunbird-pdf-player.js");
+  await fs.copy("./dist/pdf-player-wc/assets", "web-component-demo/assets");
+  await fs.copy("./dist/pdf-player-wc/styles.css", "web-component-demo/styles.css")
   console.log("Files concatenated successfully!!!");
 };
 build();
